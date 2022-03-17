19f57045152a2855d56025a34ccecff5e50777e9
albenchris <albenchris00@gmail.com>
2022-03-17 16:32:11 -0500

    separated product buying into it's own function, found way to find upgrades without slowing down system too much

--- main.py
+++ main.py
@@ -22,22 +22,54 @@ driver.implicitly_wait(5)
 actions = ActionChains(driver)
 
 cookie = driver.find_element(By.ID, "bigCookie")
-items = []
 
 count = 0
 
-while count < 50:
+
+def buy_product():
+    products = [
+        driver.find_element(
+            By.ID,
+            "productPrice" + str(i)
+        ) for i in range(1, -1, -1)
+    ]
+    for product in products:
+        value = int(product.text)
+        if value <= count:
+            print("products", products)
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(product)
+            upgrade_actions.click()
+            upgrade_actions.perform()
+
+
+def buy_upgrade():
+    if count >= 100:
+        try:
+            upgrades = driver.find_elements(By.CLASS_NAME, "upgrade")
+            print("upgrades", upgrades)
+        except:
+            i = 1
+
+
+def click_golden_cookie():
+    golden_cookie = driver.find_element(By.CLASS_NAME, "shimmer")
+
+while count < 120:
     # cookie.click()
     actions.click(cookie)
 
-    items = list(dict.fromkeys([driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1, -1, -1)]))
-    for item in items:
-        value = int(item.text)
-        if value <= count:
-            upgrade_actions = ActionChains(driver)
-            upgrade_actions.move_to_element(item)
-            upgrade_actions.click()
-            upgrade_actions.perform()
+    buy_product()
+    buy_upgrade()
+
+    # items = list(dict.fromkeys([driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1, -1, -1)]))
+    # for item in items:
+    #     value = int(item.text)
+    #     if value <= count:
+    #         upgrade_actions = ActionChains(driver)
+    #         upgrade_actions.move_to_element(item)
+    #         upgrade_actions.click()
+    #         upgrade_actions.perform()
 
     actions.perform()
     count = int(driver.find_element(By.ID, "cookies").text.split(" ")[0])
