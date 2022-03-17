0debe97b811635ac3a0a01cccee5e51e442a0d31
albenchris <albenchris00@gmail.com>
2022-03-17 10:52:47 -0500

    added options to webdriver

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ from selenium.webdriver.common.keys import Keys
 
 from time import sleep
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--incognito")
+
 s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s, options=chrome_options)
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
