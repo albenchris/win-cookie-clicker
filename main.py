2167bcdebce78665c35d529468c8e463bff393b9
albenchris <albenchris00@gmail.com>
2022-03-17 15:53:42 -0500

    using action chains for smoother operation, automatically buys products when available

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -18,21 +19,27 @@ driver = webdriver.Chrome(service=s, options=chrome_options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.implicitly_wait(5)
 
+actions = ActionChains(driver)
+
 cookie = driver.find_element(By.ID, "bigCookie")
 items = []
 
 count = 0
 
 while count < 50:
-    cookie.click()
+    # cookie.click()
+    actions.click(cookie)
 
-    try:
-        items = driver.find_elements(By.CLASS_NAME, "enabled")
-        for item in items[::-1]:
-            item.click()
-    except:
-        print("Not enough cookies")
+    items = list(dict.fromkeys([driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1, -1, -1)]))
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
 
+    actions.perform()
     count = int(driver.find_element(By.ID, "cookies").text.split(" ")[0])
 
 sleep(2)
